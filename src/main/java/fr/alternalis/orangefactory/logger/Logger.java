8a7d6d6df5201b226260a772f28a5b1718c13528
Nexusoid <16sangoku@gmail.com>
2021-05-31 14:01:57 +0200

    Added log system.

--- src/main/java/fr/alternalis/orangefactory/logger/Logger.java
+++ src/main/java/fr/alternalis/orangefactory/logger/Logger.java
@@ -1,5 +1,60 @@
 package fr.alternalis.orangefactory.logger;
 
+import fr.alternalis.orangefactory.elements.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logger {
 
+    private File logFile = null;
+    private FileWriter writer = null;
+
+    public void openFile(){
+        try {
+            if(logFile != null){
+                writer.close();
+            }
+            logFile = new File(Parameter.logFile);
+            if (logFile.createNewFile()) {
+                System.out.println("File created: " + logFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            writer = new FileWriter(logFile, true);
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    //TODO: Add execution time
+    public void writeLog(String type, String element, String information){
+        try {
+            switch (type) {
+                case "State":
+                    writer.write("# State #" + "[ " + element + " ]" + information);
+                    break;
+                case "Info":
+                    writer.write("? Info ?" + "[ " + element + " ]" + information);
+                    break;
+                case "Action":
+                    writer.write("> Action <" + "[ " + element + " ]" + information);
+                    break;
+                case "Event":
+                    writer.write("! Event !" + "[ " + element + " ]" + information);
+                    break;
+                case "Error":
+                    writer.write("< Error >" + "[ " + element + " ]" + information);
+                    break;
+                case "Result":
+                    writer.write("$ Result $" + "[ " + element + " ]" + information);
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
